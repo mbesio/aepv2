c13ef5f5d0c3d4d6466f763efc3ac87417cd31a0
Matteo <matteo.besio@gmail.com>
2019-04-17 16:54:45 -0700

    Failing test number 3 scissorsShouldWinOverPaper

--- test/edu/berkeley/aep/Tests.java
+++ test/edu/berkeley/aep/Tests.java
@@ -20,5 +20,12 @@ public class Tests {
         assertEquals(paper.and(rock), paper);
     }
 
+    @Test
+    public void ScissorsShouldWinOverPaper() {
+        var scissors = new Player(Unit.SCISSORS);
+        var paper = new Player(Unit.PAPER);
+        assertEquals(scissors.and(paper), scissors);
+    }
+
 
 }
